6b0fe175f95be18df849542614b477657c45845f
Anushka Shukla <anushka.shukla1708@gmail.com>
2026-08-01 03:23:42 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import VerifyOTP from "./pages/VerifyOTP";
+import VerifyOTP from "./pages/verifyOTP";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
