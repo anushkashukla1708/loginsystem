b890809c9c2ad9711941d129d5264b2d3e29fbe2
Anushka Shukla <anushka.shukla1708@gmail.com>
2026-08-01 05:00:39 +0530

    done

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://loginsystemreg1.vercel.app",
+      "https://loginsystemorg.vercel.app",
     ],
     credentials: true,
   })
