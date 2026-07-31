7f823b81256ec35f09fd9ee325b159579b08672e
Anushka Shukla <anushka.shukla1708@gmail.com>
2026-08-01 03:27:11 +0530

    final

--- server/server.js
+++ server/server.js
@@ -14,7 +14,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://loginsystemreg1.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
