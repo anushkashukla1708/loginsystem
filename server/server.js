2952fd643a0a87ba2a96f188dccf329cec919605
Anushka Shukla <anushka.shukla1708@gmail.com>
2026-08-02 13:21:03 +0530

    Switch to Brevo SMTP

--- server/server.js
+++ server/server.js
@@ -1,46 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const dns = require("dns");
+const nodemailer = require("nodemailer");
 
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes");
-
-dotenv.config();
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-connectDB();
-
-const app = express();
-
-// Middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://loginsystem12345.vercel.app",
-      "https://loginsystem12345-git-main-anushka-self.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Server Running...");
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+const sendEmail = async (email, otp) => {
+  await transporter.sendMail({
+    from: process.env.BREVO_USER,
+    to: email,
+    subject: "OTP Verification",
+    html: `
+      <h2>MERN Authentication</h2>
+      <p>Your OTP is:</p>
+      <h1>${otp}</h1>
+      <p>This OTP is valid for 5 minutes.</p>
+    `,
+  });
+};
 
-// Start Server
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = sendEmail;
